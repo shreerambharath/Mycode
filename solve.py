61e5158f7c7ef90e3732827293744547245e86a1
User <user.push@hackerrank.workspace>
2020-11-22 21:25:32 +0000

    Auto save changes
    Commit by HackerRank

--- solve.py
+++ solve.py
@@ -1,7 +1,11 @@
 #import packages
-
+import pandas as pd
 def create_dataframe():
     #write your code here
+    odd=pd.Series(list(range(0,100,2)))
+    even=pd.Series(list(range(1,101,2)))
+    result=pd.DataFrame({'Odd': odd, 'Even': even,'Sum': odd+even})
+    result.to_csv('result.csv')
     #write to result.csv
     #return the dataframe
-    return 
+    return result
